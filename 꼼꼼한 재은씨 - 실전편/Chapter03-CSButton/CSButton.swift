1a04faa8fe8051fa3209db4b59242d2dfd8714ab
Jae-eun <je6752@gmail.com>
2020-05-11 01:33:52 +0900

    CSButton

--- 꼼꼼한 재은씨 - 실전편/Chapter03-CSButton/CSButton.swift
+++ 꼼꼼한 재은씨 - 실전편/Chapter03-CSButton/CSButton.swift
@@ -9,6 +9,11 @@
 import Foundation
 import UIKit
 
+public enum CSButtonType {
+    case rect
+    case circle
+}
+
 class CSButton: UIButton {
 
     required init(coder aDecoder: NSCoder) {
@@ -31,5 +36,52 @@ class CSButton: UIButton {
 
     init() {
         super.init(frame: CGRect.zero)
+    }
+
+    convenience init(type: CSButtonType) {
+        self.init()
+
+        switch type {
+        case .rect:
+            self.backgroundColor = .black
+            self.layer.borderColor = UIColor.black.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 0
+            self.setTitleColor(.white, for: .normal)
+            self.setTitle("Rect Button", for: .normal)
+        case .circle:
+            self.backgroundColor = .red
+            self.layer.borderColor = UIColor.blue.cgColor
+            self.layer.borderWidth = 2
+            self.layer.cornerRadius = 50
+            self.setTitle("Circle Button", for: .normal)
+        }
+
+        self.addTarget(self, action: #selector(counting(_:)), for: .touchUpInside)
+    }
+
+    var style: CSButtonType = .rect {
+        didSet {
+            switch style {
+            case .rect:
+                self.backgroundColor = .black
+                self.layer.borderColor = UIColor.black.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0
+                self.setTitleColor(.white, for: .normal)
+                self.setTitle("Rect Button", for: .normal)
+            case .circle:
+                self.backgroundColor = .red
+                self.layer.borderColor = UIColor.blue.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 50
+                self.setTitle("Circle Button", for: .normal)
+            }
+        }
+    }
+
+    @objc func counting(_ sender: UIButton) {
+        sender.tag = sender.tag + 1
+        sender.setTitle("\(sender.tag) 번째 클릭", for: .normal)
     }
 }
--- 꼼꼼한 재은씨 - 실전편/Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
+++ 꼼꼼한 재은씨 - 실전편/Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
@@ -13,9 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let frame = CGRect(x: 30, y: 50, width: 150, height: 30)
-        let csBtn = CSButton(frame: frame)
-        self.view.addSubview(csBtn)
+        // 코드로 만드는 버튼
+//        let frame = CGRect(x: 30, y: 50, width: 150, height: 30)
+//        let csBtn = CSButton(frame: frame)
+//        self.view.addSubview(csBtn)
+
+        let rectBtn = CSButton(type: .rect)
+        rectBtn.frame = CGRect(x: 30, y: 200, width: 150, height: 30)
+        self.view.addSubview(rectBtn)
+
+        let circleBtn = CSButton(type: .circle)
+        circleBtn.frame = CGRect(x: 200, y: 200, width: 150, height: 30)
+        self.view.addSubview(circleBtn)
+
+        circleBtn.style = .rect
     }
 
 
