53b1fde8c6f6a8e584b642679e123aaa42430a11
Jae-eun <je6752@gmail.com>
2020-07-19 23:16:18 +0900

    20200719

---
+++ 꼼꼼한 재은씨 - 실전편/imsi/Chapter06-HR/Chapter06-HR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by 이재은 on 2020/07/19.
+//  Copyright © 2020 Jaeeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
