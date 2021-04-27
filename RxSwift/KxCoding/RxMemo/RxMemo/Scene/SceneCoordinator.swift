c809a693c1c0966868a0c124987df721fcab1f85
Jae-eun <je6752@gmail.com>
2021-04-27 22:25:24 +0900

    MemoList Scene : RxMemo

--- RxSwift/KxCoding/RxMemo/RxMemo/Scene/SceneCoordinator.swift
+++ RxSwift/KxCoding/RxMemo/RxMemo/Scene/SceneCoordinator.swift
@@ -72,4 +72,3 @@ class SceneCoordinator: SceneCoordinatorType {
         }
     }
 }
-}
--- RxSwift/KxCoding/RxMemo/RxMemo/ViewController/MemoListViewController.swift
+++ RxSwift/KxCoding/RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -6,20 +6,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
-
+    
+    private let disposeBag = DisposeBag()
     var viewModel: MemoListViewModel!
-
-
+    
+    @IBOutlet private weak var listTableView: UITableView!
+    @IBOutlet private weak var addBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     func bindViewModel() {
-
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "memoListCell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: disposeBag)
     }
-
+    
 }
